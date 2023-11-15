41f903a92fd58a138fb40ee6364ee9ad9afe892d
LKV <a23202418@gmail.com>
2023-11-15 17:53:26 +0800

    fix: update fill_the_array

--- 5/practice/fill_the_array/fill_the_array.c
+++ 5/practice/fill_the_array/fill_the_array.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 void fill_array(int *ptr[], int n);
- 
+
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
@@ -23,38 +23,16 @@ int main() {
         printf("%d%c", arr[i], " \n"[i == m - 1]);
     return 0;
 }
- 
-void fill_array(int *ptr[], int n) {
-    int length = ptr[n - 1] - ptr[0] + 1;
-    int position[length], front = 0;
-    int i = 0;
-    while (i < length) {
-        *(ptr[0] + i) = 0;
-        i++;
-    }
-    i = 0;
-    while (i < n) {
+
+int cmp(const void *a, const void *b) { return *(int **)a - *(int **)b; }
+
+void fill_array(int *ptr[], int size) {
+    for (int i = 0; i < size; i++)
         *ptr[i] = i;
-        i++;
-    }
-    i = 0;
-    while (i < length) {
-        if (*(ptr[0] + i) != 0) {
-            front = *(ptr[0] + i);
-            position[i] = 0;
-        }
-        if (*(ptr[0] + i) == 0) {
-            *(ptr[0] + i) += front;
-            position[i] = 1;
-        }
-        i++;
-    }
-    i = length - 1;
-    while (i > 0) {
-        if (position[i] == 0)
-            front = *(ptr[0] + i);
-        if (position[i] == 1)
-            *(ptr[0] + i) += front;
-        i--;
-    }
+
+    qsort(ptr, size, sizeof(int *), cmp);
+    
+    for (int i = 0; i < size - 1; i++)
+        for (int *j = ptr[i] + 1; j < ptr[i + 1]; j++)
+            *j = *ptr[i] + *ptr[i + 1];
 }
